33212f2e108b40894430cb923f690ddcd5530059
Marianne Santos <20213tn140@utez.edu.mx>
2024-11-21 21:59:42 +0000

    [UPDATE] Home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,7 +2,7 @@
 <template>
     <div>
       <h1>Home</h1>
-      <p>Bienvenido a la página de inicio.</p>
+      <p>Bienvenido a la página de inicio probando webhook.</p>
     </div>
   </template>
   
